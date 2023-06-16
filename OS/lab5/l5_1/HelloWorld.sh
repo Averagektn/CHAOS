bd7d8acc3390f07168a53a34b8b04d31c0c7a01e
Никита Глушаченко <90308369+Averagektn@users.noreply.github.com>
2023-06-16 16:54:55 +0300

    Delete OS directory

--- OS/lab5/l5_1/HelloWorld.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find $3 -type f -size +$1 -size -$2 -printf 'PATH: %h\nSIZE: %s\nFILE NAME: %f\n\n' | sed -n 1,79p
--- OS/lab5/l5_1/L5_1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find $3 -type f -size +$1 -size -$2 -printf "PATH: %h SIZE: %s FILE NAME: %f\n" | sed 1,20p
